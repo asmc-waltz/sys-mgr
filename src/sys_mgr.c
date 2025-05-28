d55fbfd4a7a6a8fb95303e65b642d0242d092268
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-05-28 23:09:21 +0700

    sys-mgr: improve fail case handling in main function
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/sys_mgr.c
+++ src/sys_mgr.c
@@ -41,17 +41,17 @@ int setup_signal_handler()
 {
     if (signal(SIGINT, sig_handler) == SIG_ERR) {
         LOG_ERROR("Error registering signal SIGINT handler");
-        return EINVAL;
+        return EXIT_FAILURE;
     }
 
     if (signal(SIGTERM, sig_handler) == SIG_ERR) {
         LOG_ERROR("Error registering signal SIGTERM handler");
-        return EINVAL;
+        return EXIT_FAILURE;
     }
 
     if (signal(SIGABRT, sig_handler) == SIG_ERR) {
         LOG_ERROR("Error registering signal SIGABRT handler");
-        return EINVAL;
+        return EXIT_FAILURE;
     }
 
     return 0;
@@ -75,45 +75,59 @@ int main() {
     int ret = 0;
 
     if (setup_signal_handler()) {
-        return EINVAL;
+        goto exit_error;
     }
 
     conn = setup_dbus();
     if (!conn) {
         LOG_FATAL("System Manager: Unable to establish connection with DBus");
-        return -1;
+        goto exit_error;
     }
 
     ret = pthread_create(&task_handler, NULL, main_task_handler, NULL);
     if (ret) {
         LOG_FATAL("Failed to create worker thread: %s", strerror(ret));
-        exit(1);
+        goto exit_dbus;
     }
 
     // Prepare eventfd to notify epoll when communicating with a thread
     ret = init_event_file();
     if (ret) {
         LOG_FATAL("Failed to initialize eventfd");
+        goto exit_workqueue;
     }
 
     // This thread processes DBus messages for the system manager
     ret = pthread_create(&dbus_listener, NULL, dbus_listen_thread, conn);
     if (ret) {
         LOG_FATAL("Failed to create DBus listen thread: %s", strerror(ret));
-        exit(1);
+        goto exit_workqueue;
     }
 
     // System manager's primary tasks are executed within a loop
     ret = main_loop();
     if (ret) {
-        event_set(event_fd, SIGUSR1);
-        workqueue_stop();
+        goto exit_listener;
     }
 
     pthread_join(dbus_listener, NULL);
     pthread_join(task_handler, NULL);
-
+    dbus_connection_unref(conn);
     close(event_fd);
     LOG_DEBUG("All services stopped. Safe exit.\n");
-    return 0;
+    return EXIT_SUCCESS;
+
+exit_listener:
+    event_set(event_fd, SIGUSR1);
+    pthread_join(dbus_listener, NULL);
+
+exit_workqueue:
+    workqueue_stop();
+    pthread_join(task_handler, NULL);
+
+exit_dbus:
+    dbus_connection_unref(conn);
+
+exit_error:
+    return EXIT_FAILURE;
 }
