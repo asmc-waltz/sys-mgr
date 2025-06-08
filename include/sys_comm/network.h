9bf62a60f14dac234a9b7b61e8225af5502515f7
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-06-08 22:12:40 +0700

    wifi: support function to scan and get wifi info for iface
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- include/sys_comm/network.h
+++ include/sys_comm/network.h
@@ -6,9 +6,13 @@
 #ifndef G_NETWORK_H
 #define G_NETWORK_H
 
+#include <NetworkManager.h>
+
 int network_manager_comm_init();
 void network_manager_comm_deinit();
 
-int scan_wifi();
+NMDevice * g_nm_device_get_by_iface(const char *exp_iface);
+
+int wifi_scan_and_get_results(const char *iface, int scan);
 
 #endif /* G_NETWORK_H */
--- src/comm/sys_comm/net/wifi.c
+++ src/comm/sys_comm/net/wifi.c
@@ -1,59 +1,90 @@
 #include <stdio.h>
-
 #include <glib.h>
 #include <NetworkManager.h>
 
 #include <log.h>
 #include <sys_comm.h>
 
-int scan_wifi()
+int wifi_scan_and_get_results(const char *iface, int scan)
 {
+    char *ssid_str = NULL;
+    NMAccessPoint *ap = NULL;
+    const guint8 *ssid = NULL;
     g_autoptr(GError) error = NULL;
-    NMClient *client = get_nm_client();
 
-    const GPtrArray *devices = nm_client_get_devices(client);
+    NMDevice *dev = g_nm_device_get_by_iface(iface);
+    if (!NM_IS_DEVICE_WIFI(dev)) {
+        LOG_ERROR("Wrong device type: %s\n", nm_device_get_iface(dev));
+        return EXIT_FAILURE;
+    } else {
+        LOG_DEBUG("WiFi device: %s\n", nm_device_get_iface(dev));
+    }
 
-    for (guint i = 0; i < devices->len; ++i) {
-        NMDevice *device = g_ptr_array_index(devices, i);
+    NMDeviceWifi *wifi_device = NM_DEVICE_WIFI(dev);
 
-        if (NM_IS_DEVICE_WIFI(device)) {
-            NMDeviceWifi *wifi_device = NM_DEVICE_WIFI(device);
+    if (scan) {
+        nm_device_wifi_request_scan(wifi_device, NULL, &error);
+        g_usleep(3000000);
+    }
 
-            LOG_TRACE("Found WiFi device: %s\n", nm_device_get_iface(device));
+    const GPtrArray *aps = nm_device_wifi_get_access_points(wifi_device);
+    LOG_TRACE("Found %u access points:\n", aps->len);
 
-            // Optional: request scan
-            nm_device_wifi_request_scan(wifi_device, NULL, &error);
-
-            // Wait a bit for scan to complete
-            g_usleep(5000000); // 2 seconds
-
-            const GPtrArray *aps = nm_device_wifi_get_access_points(wifi_device);
-
-            LOG_TRACE("Found %u access points:\n", aps->len);
-
-            for (guint j = 0; j < aps->len; ++j) {
-                NMAccessPoint *ap = g_ptr_array_index(aps, j);
-
-                const guint8 *ssid = nm_access_point_get_ssid(ap);
-
-                char *ssid_str; // = nm_utils_ssid_to_utf8(ssid, 100);
-                if (ssid) {
-                    ssid_str = nm_utils_ssid_to_utf8(g_bytes_get_data(ssid, NULL), g_bytes_get_size(ssid));
-                }
-                // char *ssid_str = nm_utils_ssid_to_utf8(ssid, 100);
-
-                int strength = nm_access_point_get_strength(ap);
-                NM80211ApSecurityFlags sec_flags = nm_access_point_get_flags(ap);
-
-                LOG_TRACE("SSID: %-30s Strength: %3d%% SecurityFlags: 0x%x",
-                        ssid_str ? ssid_str : "<hidden>",
-                        strength,
-                        sec_flags);
-
-                // g_free(ssid_str);
-            }
+    for (guint j = 0; j < aps->len; ++j) {
+        ap = g_ptr_array_index(aps, j);
+        ssid = nm_access_point_get_ssid(ap);
+        if (!ssid) {
+            continue;
         }
+
+        ssid_str = nm_utils_ssid_to_utf8(g_bytes_get_data(ssid, NULL), \
+                                         g_bytes_get_size(ssid));
+
+        int strength = nm_access_point_get_strength(ap);
+        NM80211ApSecurityFlags sec_flags = nm_access_point_get_flags(ap);
+
+        LOG_TRACE("SSID: %-30s Strength: %3d%% SecurityFlags: 0x%x",
+                ssid_str ? ssid_str : "<hidden>",
+                strength,
+                sec_flags);
+
+        g_free(ssid_str);
     }
 
     return EXIT_SUCCESS;
 }
+
+int wifi_connected_ap_get(const char *iface)
+{
+    char *ssid_str = NULL;
+    const guint8 *ssid = NULL;
+    NMDeviceWifi *wifi_dev = NULL;
+    NMAccessPoint *active_ap = NULL;
+    NMDevice *dev = g_nm_device_get_by_iface(iface);
+
+    if (!NM_IS_DEVICE_WIFI(dev)) {
+        LOG_ERROR("Wrong device type: %s\n", nm_device_get_iface(dev));
+        return EXIT_FAILURE;
+    } else {
+        LOG_DEBUG("WiFi device: %s\n", nm_device_get_iface(dev));
+    }
+
+    wifi_dev = NM_DEVICE_WIFI(dev);
+    active_ap = nm_device_wifi_get_active_access_point(wifi_dev);
+    if (active_ap) {
+        ssid = nm_access_point_get_ssid(active_ap);
+        if (!ssid) {
+            return EXIT_FAILURE;
+        }
+        ssid_str = nm_utils_ssid_to_utf8(g_bytes_get_data(ssid, NULL), \
+                                             g_bytes_get_size(ssid));
+        LOG_TRACE("Wi-Fi interface [%s] connected to AP: [%s]\n", iface, \
+                ssid_str ? ssid_str : "<hidden>");
+        g_free(ssid_str);
+    } else {
+        LOG_TRACE("Wi-Fi interface [%s] is not connected to any AP", iface);
+    }
+
+    return EXIT_SUCCESS;
+}
+
