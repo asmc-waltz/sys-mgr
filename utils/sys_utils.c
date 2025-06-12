1ecdf22b47c50e484b5e3502d47ad23cdcbbcd52
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-06-12 17:44:15 +0700

    dbus: test: remove redundant code for signal and method handler
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- utils/sys_utils.c
+++ utils/sys_utils.c
@@ -127,11 +127,12 @@ bool decode_data_frame(DBusMessage *msg, DataFrame *out)
     return true;
 }
 
-void create_sample_frame(DataFrame *frame) {
+// Create a method frame to send via method call
+void create_method_frame(DataFrame *frame)
+{
     frame->component_id = "terminal-ui";
     frame->topic_id = 1001;
     frame->opcode = 42;
-
     frame->entry_count = 2;
 
     frame->entries[0].key = "username";
@@ -145,21 +146,45 @@ void create_sample_frame(DataFrame *frame) {
     frame->entries[1].value.i32 = 30;
 }
 
+// Create a sample frame to send as a signal
+void create_signal_frame(DataFrame *frame)
+{
+    frame->component_id = "terminal-ui";
+    frame->topic_id = 1001;
+    frame->opcode = 42;
+    frame->entry_count = 2;
 
-int send_method_call(DBusConnection* conn) {
+    frame->entries[0].key = "username";
+    frame->entries[0].data_type = DBUS_TYPE_STRING;
+    frame->entries[0].data_length = 0;
+    frame->entries[0].value.str = "haha";
+
+    frame->entries[1].key = "age";
+    frame->entries[1].data_type = DBUS_TYPE_INT32;
+    frame->entries[1].data_length = 0;
+    frame->entries[1].value.i32 = 31;
+}
+
+// Send a DBus method call with encoded DataFrame
+int send_method_call(DBusConnection *conn)
+{
     DBusMessage *msg;
-    DBusMessageIter args;
     DBusPendingCall *pending;
-
-    msg = dbus_message_new_method_call(SYS_MGR_DBUS_SER, \
-                                       SYS_MGR_DBUS_OBJ_PATH, \
-                                       SYS_MGR_DBUS_IFACE, \
-                                       SYS_MGR_DBUS_METH);
-
-
-    /////////////////////////////////////////////////
+    DBusMessage *reply;
+    DBusMessageIter reply_args;
     DataFrame frame;
-    create_sample_frame(&frame);
+    int ret = EXIT_SUCCESS;
+
+    msg = dbus_message_new_method_call(SYS_MGR_DBUS_SER,
+                                       SYS_MGR_DBUS_OBJ_PATH,
+                                       SYS_MGR_DBUS_IFACE,
+                                       SYS_MGR_DBUS_METH);
+    if (!msg) {
+        LOG_FATAL("Failed to create method call message");
+        return EXIT_FAILURE;
+    }
+
+    create_method_frame(&frame);
 
     if (!encode_data_frame(msg, &frame)) {
         LOG_ERROR("Failed to encode data frame");
@@ -167,19 +192,9 @@ int send_method_call(DBusConnection* conn) {
         return EXIT_FAILURE;
     }
 
-    /////////////////////////////////////////////////
-        /*
-    dbus_message_iter_init_append(msg, &args);
-
-    const char *str = "TERMINAL_UI";
-    int opcode = 0;
-    dbus_message_iter_append_basic(&args, DBUS_TYPE_STRING, &str);
-    dbus_message_iter_append_basic(&args, DBUS_TYPE_INT32, &opcode);
-        */
-    /////////////////////////////////////////////////
-
     if (!dbus_connection_send_with_reply(conn, msg, &pending, -1)) {
-        LOG_ERROR("Out of memory!");
+        LOG_ERROR("Out of memory while sending method call");
+        dbus_message_unref(msg);
         return EXIT_FAILURE;
     }
 
@@ -187,125 +202,103 @@ int send_method_call(DBusConnection* conn) {
     dbus_message_unref(msg);
 
     dbus_pending_call_block(pending);
-
-    DBusMessage *reply = dbus_pending_call_steal_reply(pending);
+    reply = dbus_pending_call_steal_reply(pending);
     dbus_pending_call_unref(pending);
 
-    if (reply) {
-        DBusMessageIter reply_args;
-        if (dbus_message_iter_init(reply, &reply_args)) {
-            char *reply_str;
-            dbus_message_iter_get_basic(&reply_args, &reply_str);
-            LOG_INFO("Method call reply: %s", reply_str);
-        }
-        dbus_message_unref(reply);
+    if (!reply) {
+        LOG_ERROR("No reply received for method call");
+        return EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    if (dbus_message_iter_init(reply, &reply_args)) {
+        char *reply_str = NULL;
+        dbus_message_iter_get_basic(&reply_args, &reply_str);
+        if (reply_str) {
+            LOG_INFO("Method call reply: %s", reply_str);
+        } else {
+            LOG_WARN("Method call reply received but empty");
+        }
+    } else {
+        LOG_WARN("Reply does not contain arguments");
+    }
+
+    dbus_message_unref(reply);
+    return ret;
 }
 
-int send_signal(DBusConnection* conn) {
-    DBusMessage* msg;
-    DBusMessageIter args, struct_iter, dict_iter, entry_iter;
+// Send a DBus signal with encoded DataFrame
+int send_signal(DBusConnection *conn)
+{
+    DBusMessage *msg;
+    DataFrame frame;
 
-    const char* name = "TERMINAL_UI";
-    int32_t opcode = 23;
-    const char* key1 = "SSID";
-    const char* val1 = "local_network";
-    const char* key2 = "PASSWORD";
-    const char* val2 = "123456";
-    const char* key3 = "AUTO_CONNECT";
-    const char* val3 = "1";
-    const char* extra = "private network";
-
-
-    msg = dbus_message_new_signal(UI_DBUS_OBJ_PATH, \
-                                  UI_DBUS_IFACE, \
+    msg = dbus_message_new_signal(UI_DBUS_OBJ_PATH,
+                                  UI_DBUS_IFACE,
                                   UI_DBUS_SIG);
     if (!msg) {
-        fprintf(stderr, "Message NULL\n");
-        exit(1);
+        LOG_FATAL("Failed to create signal message");
+        return EXIT_FAILURE;
     }
 
-    dbus_message_iter_init_append(msg, &args);
+    create_signal_frame(&frame);
 
-    // Begin struct (si a{ss} s)
-    dbus_message_iter_open_container(&args, DBUS_TYPE_STRUCT, NULL, &struct_iter);
-
-    dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_STRING, &name);
-    dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_INT32, &opcode);
-
-    // Begin dict
-    dbus_message_iter_open_container(&struct_iter, DBUS_TYPE_ARRAY, "{ss}", &dict_iter);
-    // Entry 1
-    dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_DICT_ENTRY, NULL, &entry_iter);
-    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &key1);
-    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &val1);
-    dbus_message_iter_close_container(&dict_iter, &entry_iter);
-    // Entry 2
-    dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_DICT_ENTRY, NULL, &entry_iter);
-    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &key2);
-    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &val2);
-    dbus_message_iter_close_container(&dict_iter, &entry_iter);
-    // Entry 3
-    dbus_message_iter_open_container(&dict_iter, DBUS_TYPE_DICT_ENTRY, NULL, &entry_iter);
-    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &key3);
-    dbus_message_iter_append_basic(&entry_iter, DBUS_TYPE_STRING, &val3);
-    dbus_message_iter_close_container(&dict_iter, &entry_iter);
-
-    dbus_message_iter_close_container(&struct_iter, &dict_iter);
-
-    // Extra string
-    dbus_message_iter_append_basic(&struct_iter, DBUS_TYPE_STRING, &extra);
-
-    dbus_message_iter_close_container(&args, &struct_iter);
+    if (!encode_data_frame(msg, &frame)) {
+        LOG_ERROR("Failed to encode data frame");
+        dbus_message_unref(msg);
+        return EXIT_FAILURE;
+    }
 
     if (!dbus_connection_send(conn, msg, NULL)) {
-        LOG_ERROR("Out of memory!");
+        LOG_ERROR("Out of memory while sending signal");
+        dbus_message_unref(msg);
         return EXIT_FAILURE;
     }
 
     dbus_connection_flush(conn);
     dbus_message_unref(msg);
-    LOG_INFO("Signal sent.");
 
+    LOG_INFO("Signal sent successfully");
     return EXIT_SUCCESS;
 }
 
-int main(int argc, char** argv) {
+// Main entry point of the CLI test application
+int main(int argc, char **argv)
+{
     DBusError err;
-    DBusConnection* conn;
-    int ret = 0;
+    DBusConnection *conn;
+    int ret;
 
     dbus_error_init(&err);
     conn = dbus_bus_get(DBUS_BUS_SYSTEM, &err);
-
     if (dbus_error_is_set(&err)) {
-        LOG_ERROR("Connection Error (%s)", err.message);
+        LOG_ERROR("Connection error: %s", err.message);
         dbus_error_free(&err);
     }
 
     if (!conn) {
+        LOG_FATAL("Failed to connect to DBus system bus");
         return EXIT_FAILURE;
     }
 
-    ret = dbus_bus_request_name(conn, \
-                                UI_DBUS_SER, \
-                                DBUS_NAME_FLAG_REPLACE_EXISTING, \
+    ret = dbus_bus_request_name(conn,
+                                UI_DBUS_SER,
+                                DBUS_NAME_FLAG_REPLACE_EXISTING,
                                 &err);
     if (dbus_error_is_set(&err)) {
-        LOG_ERROR("Dbus request name error: %s", err.message);
+        LOG_ERROR("DBus name request error: %s", err.message);
         dbus_error_free(&err);
     }
 
     if (ret != DBUS_REQUEST_NAME_REPLY_PRIMARY_OWNER) {
+        LOG_FATAL("Not primary owner of the requested DBus name");
         return EXIT_FAILURE;
     }
 
-    if (argc > 1 && strcmp(argv[1], "signal") == 0)
-        send_signal(conn);
-    else
-        send_method_call(conn);
+    if (argc > 1 && strcmp(argv[1], "signal") == 0) {
+        return send_signal(conn);
+    } else {
+        return send_method_call(conn);
+    }
 
     return EXIT_SUCCESS;
 }
