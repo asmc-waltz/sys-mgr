ac7f34639148a2b9e5f1ccf7a13cc5fd2cba7950
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-08-21 17:08:05 +0700

    fs comm: porting: add source file to support fs comm
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- include/sys_comm.h
+++ include/sys_comm.h
@@ -12,4 +12,9 @@ int init_event_file();
 void event_set(int evfd, uint64_t code);
 uint64_t event_get(int evfd);
 
+int gf_fs_write_file(const char *path, const char *data, size_t len);
+int gf_fs_append_file(const char *path, const char *data, size_t len);
+char *gf_fs_read_file(const char *path, size_t *out_len);
+int gf_fs_file_exists(const char *path);
+
 #endif /* G_SYS_COMM_H */
