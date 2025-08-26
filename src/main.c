ef1e9cc43cbbfb0135f66dd92870159c65ebec19
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-08-26 15:35:39 +0700

    main-task: update main file based on log and coding style
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/main.c
+++ src/main.c
@@ -1,3 +1,17 @@
+/**
+ * @file main.c
+ *
+ */
+
+/*********************
+ *      INCLUDES
+ *********************/
+// #define LOG_LEVEL LOG_LEVEL_TRACE
+#if defined(LOG_LEVEL)
+#warning "LOG_LEVEL defined locally will override the global setting in this file"
+#endif
+#include <log.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,17 +22,43 @@
 #include <sys/epoll.h>
 #include <dbus/dbus.h>
 
-#include <dbus_comm.h>
-#include <sys_comm.h>
+#include <comm/dbus_comm.h>
+#include <comm/sys_comm.h>
+#include <comm/net/network.h>
+#include <sched/workqueue.h>
+#include <sched/task.h>
 
-#include <workqueue.h>
-#include <task.h>
-#include <log.h>
+/*********************
+ *      DEFINES
+ *********************/
 
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ *  GLOBAL VARIABLES
+ **********************/
 extern int event_fd;
 volatile sig_atomic_t g_run = 1;
 
-void sig_handler(int sig) {
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+/**********************
+ *  STATIC VARIABLES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+/**********************
+ *   STATIC FUNCTIONS
+ **********************/
+static void sig_handler(int32_t sig)
+{
     switch (sig) {
         case SIGINT:
             LOG_WARN("[+] Received SIGINT (Ctrl+C). Exiting...");
@@ -39,43 +79,53 @@ void sig_handler(int sig) {
     }
 }
 
-int setup_signal_handler()
+static int32_t setup_signal_handler()
 {
     if (signal(SIGINT, sig_handler) == SIG_ERR) {
         LOG_ERROR("Error registering signal SIGINT handler");
-        return EXIT_FAILURE;
+        return -1;
     }
 
     if (signal(SIGTERM, sig_handler) == SIG_ERR) {
         LOG_ERROR("Error registering signal SIGTERM handler");
-        return EXIT_FAILURE;
+        return -1;
     }
 
     if (signal(SIGABRT, sig_handler) == SIG_ERR) {
         LOG_ERROR("Error registering signal SIGABRT handler");
-        return EXIT_FAILURE;
+        return -1;
     }
 
     return 0;
 }
 
-int main_loop()
+
+static int32_t main_loop()
 {
+    uint32_t cnt = 0;
+
     LOG_INFO("System manager service is running...");
     while (g_run) {
         usleep(200000);
+        if (++cnt == 20) {
+            cnt = 0;
+            is_task_handler_idle();
+        }
     };
 
     LOG_INFO("System manager service is exiting...");
     return 0;
 }
 
-int main() {
-    DBusConnection *conn;
-    pthread_t dbus_listener;
+/**********************
+ *   GLOBAL FUNCTIONS
+ **********************/
+int32_t main(void)
+{
     pthread_t task_handler;
-    int ret = 0;
+    int32_t ret = 0;
 
+    LOG_INFO("|---------------------> SYSTEM MANAGER <----------------------|");
     if (setup_signal_handler()) {
         goto exit_error;
     }
@@ -111,9 +161,10 @@ int main() {
     pthread_join(task_handler, NULL);
     // TODO: release audio HW
     snd_sys_release();
-    close(event_fd);
-    LOG_DEBUG("All services stopped. Safe exit.\n");
-    return EXIT_SUCCESS;
+    cleanup_event_file();
+
+    LOG_INFO("|-------------> All services stopped. Safe exit <-------------|");
+    return 0;
 
 exit_nm:
     network_manager_comm_deinit();
@@ -124,7 +175,8 @@ exit_listener:
 exit_workqueue:
     workqueue_stop();
     pthread_join(task_handler, NULL);
+    cleanup_event_file();
 
 exit_error:
-    return EXIT_FAILURE;
+    return -1;
 }
