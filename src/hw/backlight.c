6222cab483e39bbddeef643f4aa093989485c1b9
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-08-27 00:27:30 +0700

    backlight: support brightness ramp function
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/hw/backlight.c
+++ src/hw/backlight.c
@@ -13,6 +13,7 @@
 #include <log.h>
 
 #include <stdint.h>
+#include <errno.h>
 
 #include <comm/f_comm.h>
 #include <hw/common.h>
@@ -21,6 +22,7 @@
  *      DEFINES
  *********************/
 #define FS_BRIGHTNESS               "/sys/class/backlight/backlight/brightness"
+#define FS_ACTUAL_BRIGHTNESS        "/sys/class/backlight/backlight/actual_brightness"
 #define FS_BRIGHTNESS_POWER         "/sys/class/backlight/backlight/bl_power"
 
 /**********************
@@ -63,16 +65,65 @@ int32_t backlight_setup()
     return ret;
 }
 
-int32_t set_brightness(uint8_t bl_peretent)
+int32_t set_brightness(uint8_t brightness)
 {
-    char str_peretent[10];
+    char str_percent[4];
+    int32_t ret = 0;
 
-    // TODO: workaround when backlight cannot be set to zero from kernel
-    if (bl_peretent == 0) {
-        bl_peretent = 1;
+    /* Workaround: kernel cannot set backlight to zero */
+    if (brightness == 0) {
+        brightness = 1;
     }
 
-    sprintf(str_peretent, "%d", bl_peretent);
-    gf_fs_write_file(FS_BRIGHTNESS, str_peretent, sizeof(str_peretent));
+    snprintf(str_percent, sizeof(str_percent), "%u", brightness);
+
+    ret = gf_fs_write_file(FS_BRIGHTNESS, str_percent, sizeof(str_percent));
+    if (ret < 0) {
+        LOG_ERROR("Failed to set brightness to %u, err=%d (%s)", brightness, \
+                  ret, strerror(errno));
+        return ret;
+    }
+
+    LOG_TRACE("Brightness set to %u", brightness);
+    return 0;
 }
 
+int32_t brightness_ramp(uint8_t from, uint8_t to, uint32_t period_us)
+{
+    uint32_t step_us;
+    uint8_t step;
+    int32_t ret = 0;
+
+    if (from == to)
+        return set_brightness(to);
+
+    step = (from > to) ? (from - to) : (to - from);
+    if (step == 0) {
+        LOG_ERROR("Invalid brightness step: from=%u, to=%u", from, to);
+        return -EINVAL;
+    }
+
+    step_us = period_us / step;
+
+    if (from < to) {
+        for (uint8_t i = from; i <= to; i++) {
+            ret = set_brightness(i);
+            if (ret < 0)
+                return ret;
+            usleep(step_us);
+        }
+    } else {
+        for (uint8_t i = from; i >= to; i--) {
+            ret = set_brightness(i);
+            if (ret < 0)
+                return ret;
+            usleep(step_us);
+            if (i == 0)
+                break; /* Prevent underflow */
+        }
+    }
+
+    LOG_DEBUG("Brightness ramp done: from=%u to=%u period=%u us", \
+              from, to, period_us);
+    return 0;
+}
