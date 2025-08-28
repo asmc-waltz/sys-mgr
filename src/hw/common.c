ad3c18942462f7804bda2418cd5ac63074280310
Phuong Nguyen <phuongnguyen0793@outlook.com>
2025-08-28 17:02:30 +0700

    hw: common: update init - deinit - monitor function
    Signed-off-by: Phuong Nguyen <phuongnguyen0793@outlook.com>

--- src/hw/common.c
+++ src/hw/common.c
@@ -51,44 +51,23 @@
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
-void test_find_iio_device(const char *find_id)
-{
-    char device_path[MAX_PATH_LEN];
-    int ret;
-
-    if (!find_id) {
-        printf("Invalid argument\n");
-        return;
-    }
-
-    ret = find_device_path_by_name(IIO_DEV_SYSFS_PATH, IIO_DEV_NAME_FILE, \
-                                   find_id, device_path, \
-                                   sizeof(device_path));
-    if (ret == 0) {
-        printf("Found IIO device: %s (target=%s)\n", device_path, find_id);
-    } else if (ret == -ENOENT) {
-        printf("Device '%s' not found\n", find_id);
-    } else {
-        printf("Error: %s\n", strerror(-ret));
-    }
-}
-
-int32_t hw_init()
+int32_t common_hardware_init()
 {
     backlight_setup();
     brightness_ramp(0, 100, 500000);
-    test_find_iio_device("opt3001");
-    test_find_iio_device("mpu6500");
-    test_find_iio_device("bmp280");
+
+    char dev_path[MAX_PATH_LEN];
+    als_late_init("opt3001", dev_path, sizeof(dev_path));
+    als_read_illuminance(dev_path);
 }
 
-int32_t hw_deinit()
+int32_t common_hardware_deinit()
 {
     backlight_setup();
     brightness_ramp(100, 0, 500000);
 }
 
-int32_t hw_monitor_loop()
+int32_t hardware_monitor_loop()
 {
     ;
 }
